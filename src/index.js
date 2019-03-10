cc8443a85c03f0d1829d55960b7756f006fbf6d1
Akshata Ambi <aksh-ambi@gmail.com>
2019-03-10 11:49:59 +0530

    Day 2 Progress with props

--- src/index.js
+++ src/index.js
@@ -1,23 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import moment from 'moment';
 
-function Avatar() {
-  return <i class="fas fa-user avatar-icon" />;
+
+function Avatar({hash}) {
+  return <img src={hash} className="avatar-icon" alt="myAvatar"/>
 }
 
-function Tweet() {
+function Tweet({tweet}) {
   return (
     <div className="tweet">
-      <Avatar />
+      <Avatar hash={tweet.gravatar} />
       <div className="content">
-        <NamewithHandle />
-        <Time/>
-        <Message />
+        <NamewithHandle author={tweet.author}/>
+        <Time  time={tweet.timestamp}/>
+        <Message text={tweet.message}/>
         <div className="buttons">
          <ReplyButton/>
-         <RetweetButton/>
-         <LikeButton/>
+         <RetweetButton retweets={tweet.retweets}/>
+         <LikeButton likes={tweet.likes}/>
          <MoreOptionsButton/>
         </div>
       </div>
@@ -25,25 +27,44 @@ function Tweet() {
   );
 }
 
-function Message() {
-  return (
-    <div className="message">Hey you are using Twitter!!</div>
+function Message({text}) {
+  return ( <div className="message">
+    {text}
+    </div>
   );
 }
 
-function NamewithHandle() {
+function NamewithHandle({author}) {
+  const {name,handle}= author;
   return (
     <span className="name-with-handle">
-      <span className="name">Akshata</span>
-      <span className="handle">@akshu</span>
+      <span className="name">{name}</span>
+      <span className="handle">@{handle}</span>
     </span>
   );
 }
 
-const Time=()=><span className="time">3h ago</span>
+const Time=({time})=>{
+const timeString = moment(time).fromNow();
+return(
+<span className="time">{timeString}</span>
+);
+}
 const ReplyButton=()=><i className="fa fa-reply reply-button"/>
-const RetweetButton=()=><i className="fa fa-retweet retweet-button"/>
-const LikeButton=()=><i className="fa fa-heart like-button"/>
+const RetweetButton=({retweets})=><div><i className="fa fa-retweet retweet-button"/>{retweets}</div>
+const LikeButton=({likes})=><div><i className="fa fa-heart like-button"/>{likes}</div>
 const MoreOptionsButton=()=><i className="fa fa-ellipsis-h more-options-button"/>
 
-ReactDOM.render(<Tweet />, document.getElementById("root"));
+var testTweet={
+  message: "Hey Akshu",
+  gravatar:"https://www.w3schools.com/howto/img_avatar2.png",
+  author:{
+    handle:"Akshata",
+    name:"@kshu"
+  },
+  likes:3,
+  retweets:0,
+  timestamp:"2019-03-08 17:41:12"
+}
+
+ReactDOM.render(<Tweet tweet={testTweet} />, document.getElementById("root"));
